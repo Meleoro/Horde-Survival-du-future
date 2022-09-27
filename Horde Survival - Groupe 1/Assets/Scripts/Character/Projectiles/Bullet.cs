bf1cb726fc9479d51ee8baaa76bfe9d5ce5d1ede
Terri <a.dubuc@rubika-edu.com>
2022-09-27 10:57:10 +0200

    Ajout package New input system + PlayerController + base bullet pooling

---
+++ Horde Survival - Groupe 1/Assets/Scripts/Character/Projectiles/Bullet.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Character.Projectiles
+{
+    public class Bullet : MonoBehaviour
+    {
+        private Rigidbody2D _rb;
+        
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.CompareTag("Wall"))
+            {
+                gameObject.SetActive(false);
+            }        
+        }
+    }
+}
+
+
