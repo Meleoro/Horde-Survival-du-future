87e160190a39b1b939109a9f97aba6f9ff5e9115
Arthur <arthur.perigno@gmail.com>
2022-09-27 18:07:01 +0200

    Avancee Upgrades

--- Horde Survival - Groupe 1/Assets/Scripts/Upgrades/Upgrade.cs
+++ Horde Survival - Groupe 1/Assets/Scripts/Upgrades/Upgrade.cs
@@ -1,5 +1,6 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
+using System.Collections.Generic;using System.Security.Claims;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,15 +9,31 @@ public class Upgrade : ScriptableObject
 {
     [Header("Upgrade / Weapon")]
     public string name;
-    public string description;
     public Image image;
     public int currentLevel;
     public bool isWeapon;
 
     [Header("If is Weapon")] 
     public GameObject weapon;
+    public List<Levels> levelList = new List<Levels>();
 
     [Header("Else")] 
     public bool damages;
     public bool health;
+}
+
+[Serializable]
+public class Levels
+{
+    public string desciption;
+
+    [Header("General")]
+    public float degats;
+    public float portee;
+    public float fireRate;
+    public int munitions;
+    public float rechargement;
+
+    [Header("UZI")] 
+    public bool doubleUZI;
 }
