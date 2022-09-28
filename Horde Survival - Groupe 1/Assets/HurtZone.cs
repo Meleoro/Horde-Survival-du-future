0f4855b9cf21fc6465785ce910152cd37214d5fb
Altana08 <l.ren@rubika-edu.com>
2022-09-28 14:28:23 +0200

    Damage test

--- Horde Survival - Groupe 1/Assets/HurtZone.cs
+++ Horde Survival - Groupe 1/Assets/HurtZone.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class HurtZone : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
+    public PlayerHealth other;
+    
+    void OnCollisionEnter2D(Collision2D col)
+             {
+                 if (col.gameObject.CompareTag("Player"))
+                 { 
+                     other.TakeDamage(1);
+                     gameObject.SetActive(false);
+                 }
+             }
 }
+
