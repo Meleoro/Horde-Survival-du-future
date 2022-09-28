b9b2a1fc744b53864ee73947241574558a8c7d1e
Terri <a.dubuc@rubika-edu.com>
2022-09-28 12:04:28 +0200

    Progress

--- Horde Survival - Groupe 1/Assets/Scripts/Character/PlayerController.cs
+++ Horde Survival - Groupe 1/Assets/Scripts/Character/PlayerController.cs
@@ -34,6 +34,7 @@ namespace Character
             _playerControls.Enable();
             
             _playerControls.Player.Movement.performed += OnMovement;
+            _playerControls.Player.Aim.performed += OnAim;
         }
         private void OnDisable()
         {
@@ -67,6 +68,10 @@ namespace Character
         {
             movement = context.ReadValue<Vector2>();
         }
+        void OnAim(InputAction.CallbackContext context)
+        {
+            _aim = context.ReadValue<Vector2>();
+        }
         void HandleMovement()
         {
             _rb.velocity = new Vector2(movement.x * playerData.characterSpeed, movement.y * playerData.characterSpeed);
@@ -74,7 +79,7 @@ namespace Character
         void HandleRotation()
         {
             float a = Mathf.Atan2(_aim.x, _aim.y) * Mathf.Rad2Deg;
-            _rb.MoveRotation(-a);
+            _rb.rotation = -a;
         }
         bool Cooldown()
         {
