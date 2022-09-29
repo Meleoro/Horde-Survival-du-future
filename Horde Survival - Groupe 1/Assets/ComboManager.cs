fe8452f4e7033d7784d203e6f5274fcaccf51c7e
Arthur <arthur.perigno@gmail.com>
2022-09-29 14:07:14 +0200

    COMBOOOOOOO

--- Horde Survival - Groupe 1/Assets/ComboManager.cs
+++ Horde Survival - Groupe 1/Assets/ComboManager.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class ComboManager : MonoBehaviour
 {
@@ -14,11 +15,21 @@ public class ComboManager : MonoBehaviour
     [Header("Références")] 
     public TextMeshProUGUI multiplier;
     public Image slider;
+    public RectTransform all;
+    public RectTransform multiplierPos;
+    public RectTransform sliderPos;
 
     [Header("Variables")] 
     private float currentMultiplier;
     private float timer;
     public static ComboManager Instance;
+    private bool canShakeAgain;
+    
+    private Vector3 pos1;
+    private Vector3 pos2;
+
+    private Vector3 originalPosMultiplier;
+    private Vector3 originalPosSlider;
 
 
     private void Awake()
@@ -26,12 +37,41 @@ public class ComboManager : MonoBehaviour
         Instance = this;
     }
 
+    private void Start()
+    {
+        pos1 = all.position;
+        pos2 = pos1 + new Vector3(10, -10, 0);
+
+        originalPosMultiplier = multiplierPos.localPosition;
+        originalPosSlider = sliderPos.localPosition;
+
+        canShakeAgain = true;
+    }
+
 
     private void Update()
     {
+        currentMultiplier = Mathf.Round(currentMultiplier * 10f) * 0.1f;
+        
         multiplier.text = "X " + currentMultiplier;
         slider.fillAmount = timer;
+
         
+        // CHANGEMENT DE POSITION
+        all.position = new Vector3(Mathf.Lerp(pos1.x, pos2.x, currentMultiplier / maxMultiplier),
+            Mathf.Lerp(pos1.y, pos2.y, currentMultiplier / maxMultiplier), 0);
+
+        // ROTATION
+        all.rotation = Quaternion.Euler(0, 0, Mathf.Lerp(0, 10, (currentMultiplier - 1)/(maxMultiplier - 1)));
+
+        // GROSSISSEMENT
+        multiplier.transform.localScale = new Vector3(Mathf.Lerp(1, 1.3f, currentMultiplier / maxMultiplier), 
+            Mathf.Lerp(1, 1.3f, currentMultiplier / maxMultiplier), 1);
+
+        if(canShakeAgain)
+            StartCoroutine(Shake());
+
+
         if (timer > 0)
         {
             timer -= (Time.deltaTime * currentMultiplier) / timerModifier;
@@ -56,5 +96,19 @@ public class ComboManager : MonoBehaviour
             currentMultiplier = maxMultiplier;
 
         timer = 1;
+    }
+
+
+    IEnumerator Shake()
+    {
+        multiplier.transform.DOShakePosition(0.1f, currentMultiplier / 2 - 0.5f);
+        
+        slider.transform.DOShakePosition(0.1f, currentMultiplier / 2 - 0.5f);
+
+        canShakeAgain = false;
+
+        yield return new WaitForSeconds(0.1f);
+
+        canShakeAgain = true;
     }
 }
